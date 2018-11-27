6023be3dbafd4e42c1fe6887b9e1bf824b785c41
CircuitSyn <circuitsyn@gmail.com>
2018-11-27 11:14:41 -0800

    added first line of seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO products (AllergyTriggerDairy, AllergyTriggerEggs, AllergyTriggerFish, AllergyTriggerSoy, AllergyTriggerSweets, AllergyTriggerNuts, AllergyTriggerWheat, PainLocationMuscular, PainLocationSkeletal, PainLocationEpidermis, SliderStressSlider, SymptomIntensitySlider, Location, Humidity, Pollen, Pollution, TimeStamp)
-VALUES (0350.00,25), (), (), (), (), (), (), (), (), ();
+VALUES (TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, TRUE, NULL , NULL, "Ankle", 7, 3, 47.620148199999996, -122.32988679999998, 24.3, 'HIGH', 'LOW', 'Tue Nov 27 2018 11:13:26 GMT-0800'), (), (), (), (), (), (), (), (), ();
 
 SELECT * FROM carddata;
