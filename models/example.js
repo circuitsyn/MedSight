b45eff61a4ae19d544ee8f3e86123576ae87aea7
CircuitSyn <circuitsyn@gmail.com>
2018-11-27 15:36:35 -0800

    fixed seeds file with SET SQL_MODE = ''; insertion

--- models/example.js
+++ models/example.js
@@ -1,23 +1,95 @@
 module.exports = function(sequelize, DataTypes) {
   var Example = sequelize.define("MedSightData", {
-    AllergyTriggerDairy: DataTypes.BOOLEAN,
-    AllergyTriggerEggs: DataTypes.BOOLEAN,
-    AllergyTriggerFish: DataTypes.BOOLEAN,
-    AllergyTriggerSoy: DataTypes.BOOLEAN,
-    AllergyTriggerSweets: DataTypes.BOOLEAN,
-    AllergyTriggerNuts: DataTypes.BOOLEAN,
-    AllergyTriggerWheat: DataTypes.BOOLEAN,
-    PainLocationMuscular: DataTypes.STRING,
-    PainLocationSkeletal: DataTypes.STRING,
-    PainLocationEpidermis: DataTypes.STRING,
-    SliderStressSlider: DataTypes.DECIMAL,
-    SymptomIntensitySlider: DataTypes.DECIMAL,
-    LocationLat: DataTypes.DECIMAL,
-    LocationLong: DataTypes.DECIMAL,
-    Humidity: DataTypes.DECIMAL,
-    Pollen: DataTypes.STRING,
-    Pollution: DataTypes.STRING,
-    TimeStamp: DataTypes.STRING
+    AllergyTriggerDairy: {
+      type: DataTypes.BOOLEAN, 
+      defaultValue: false,
+      allowNull: false
+    },
+    AllergyTriggerEggs: {
+      type: DataTypes.BOOLEAN, 
+      defaultValue: false,
+      allowNull: false
+    },
+    AllergyTriggerFish: {
+      type: DataTypes.BOOLEAN, 
+      defaultValue: false,
+      allowNull: false
+    },
+    AllergyTriggerSoy: {
+      type: DataTypes.BOOLEAN, 
+      defaultValue: false,
+      allowNull: false
+    },
+    AllergyTriggerSweets: {
+      type: DataTypes.BOOLEAN, 
+      defaultValue: false,
+      allowNull: false
+    },
+    AllergyTriggerNuts: {
+      type: DataTypes.BOOLEAN, 
+      defaultValue: false,
+      allowNull: false
+    },
+    AllergyTriggerWheat: {
+      type: DataTypes.BOOLEAN, 
+      defaultValue: false,
+      allowNull: false
+    },
+    PainLocationMuscular: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: null
+    },
+    PainLocationSkeletal: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: null
+    },
+    PainLocationEpidermis: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: null
+    },
+    SliderStressSlider: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      defaultValue: 0
+    },
+    SymptomIntensitySlider: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      defaultValue: 0
+    },
+    LocationLat: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      defaultValue: -62.200249199
+    },
+    LocationLong: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      defaultValue: -58.95916283
+    },
+    Humidity: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      defaultValue: 0
+    },
+    Pollen: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: null
+    },
+    Pollution: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: null
+    },
+    TimeStamp: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'Sat Jan 1 1983 00:00:00 GMT-0000'
+    }
   });
   return Example;
 };
