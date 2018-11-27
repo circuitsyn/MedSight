d3947415db254b61490697324391593d7cc94b20
CircuitSyn <circuitsyn@gmail.com>
2018-11-27 11:04:55 -0800

    changed Sequelize allergy types too boolean and also fixed some typos. Started Seed file

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO products (AllergyTriggerDairy, AllergyTriggerEggs, AllergyTriggerFish, AllergyTriggerSoy, AllergyTriggerSweets, AllergyTriggerNuts, AllergyTriggerWheat, PainLocationMuscular, PainLocationSkeletal, PainLocationEpidermis, SliderStressSlider, SymptomIntensitySlider, Location, Humidity, Pollen, Pollution, TimeStamp)
+VALUES (0350.00,25), (), (), (), (), (), (), (), (), ();
+
+SELECT * FROM carddata;
