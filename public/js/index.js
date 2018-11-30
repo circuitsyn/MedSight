9d0a74122ef809784ab294c0b5ad37efaa62222a
The Singer <kayla312@harbornet.com>
2018-11-29 18:32:18 -0800

    added in data ID for each slider

--- public/js/index.js
+++ public/js/index.js
@@ -108,3 +108,10 @@ var handleDeleteBtnClick = function() {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
+
+// -----------slider tool tip attempt------------//
+// $("#stressRange").slider({
+//   formatter: function(value) {
+//     return "Current value: " + value;
+//   }
+// });
