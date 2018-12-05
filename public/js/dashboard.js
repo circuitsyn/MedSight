ef8e18a0a1e633e39511ab4d5445676ca6460779
CircuitSyn <circuitsyn@gmail.com>
2018-12-04 16:45:15 -0800

    starting to build data functions

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,50 +5,57 @@ $(document).ready(function() {
         $.get("/api/cards", function(data) {
         console.log("Pain Data Request", data);
         
-        });
+        }).then(function() {
+            sympStressGraph(data);
+          });
+      }
+    
+
+    //----------------- Symp vs Stress Line Graph -------------------//
+    function sympStressGraph(data){
+        //build 3 functions here to get the data for stress, time, and sympt intensity to provide to the chart
+
+        trace1 = {
+            type: 'scatter',
+            x: time,
+            y: stressData,
+            mode: 'lines',
+            name: 'Red',
+            line: {
+            color: 'rgb(219, 64, 82)',
+            width: 3
+            }
+        };
+        
+        trace2 = {
+            type: 'scatter',
+            x: time,
+            y: symptData,
+            mode: 'lines',
+            name: 'Blue',
+            line: {
+            color: 'rgb(55, 128, 191)',
+            width: 1
+            }
+        };
+        
+        var data = [trace1, trace2];
+    
+        var layout = {
+            title: 'Pain vs Stress Over Time',
+            titlefont: {
+                family: 'MedSight Font, monospace',
+                size: 25,
+                color: '#420b56'
+            }
+        }
+        
+        Plotly.newPlot('lineGraphPainStress', data, layout);
     }
-    getPainData();
-
-    //----------------- Pain vs Stress Line Graph -------------------//
-    trace1 = {
-        type: 'scatter',
-        x: [1, 2, 3, 4],
-        y: [10, 15, 13, 17],
-        mode: 'lines',
-        name: 'Red',
-        line: {
-        color: 'rgb(219, 64, 82)',
-        width: 3
-        }
-    };
     
-    trace2 = {
-        type: 'scatter',
-        x: [1, 2, 3, 4],
-        y: [12, 9, 15, 12],
-        mode: 'lines',
-        name: 'Blue',
-        line: {
-        color: 'rgb(55, 128, 191)',
-        width: 1
-        }
-    };
-    
-    var data = [trace1, trace2];
-
-    var layout = {
-        title: 'Pain vs Stress Over Time',
-        titlefont: {
-            family: 'MedSight Font, monospace',
-            size: 25,
-            color: '#420b56'
-        }
-    }
-    
-    Plotly.newPlot('lineGraphPainStress', data, layout);
-
-    //----------- Allergy Pie Chart Based on Pain Threshold--------------//
-
+    //----------- Allergy Pie Chart Based on Pain Threshold Function --------------//
+    function allergyPie(){
+        
     var data = [{
         values: [19, 26, 55],
         labels: ['Residential', 'Non-Residential', 'Utility'],
@@ -56,5 +63,11 @@ $(document).ready(function() {
     }];
     
     Plotly.newPlot('pieChartAllergy', data);
+
+    };
+ 
+    
+//Start Drawing Graphs
+getPainData();
 
 });
