37342ee90728ad4c62f330e7268b32bf0e5401b2
Keo <kthorng@gmail.com>
2018-12-05 22:05:55 -0800

    validation

---
+++ public/js/cards.js
@@ -0,0 +1,9 @@
+/* ========================= Style Cards Page Start ============================= */
+$("img#false").hide();
+$("li#null").hide();
+function slideLock() {
+  document.getElementById("stressRangeCard").disabled = true;
+  document.getElementById("symptomRangeCard").disabled = true;
+};
+slideLock();
+/* ========================= Style Cards Page End =============================== */
--- public/js/index.js
+++ public/js/index.js
@@ -141,7 +141,10 @@ var handleFormSubmit = function(event) {
     Notes: notesInput.val().trim(),
     TimeStamp: autoTime.text()
   };
-
+  if ((medsightdata.PainLocationMuscular == medsightdata.PainLocationSkeletal) && (medsightdata.PainLocationSkeletal == medsightdata.PainLocationEpidermis)) {
+    $('#validator').modal('toggle');
+    return;
+  }
   // Add our new card
   API.saveCard(medsightdata).then(function() {
     // Goes to cards page after submission
